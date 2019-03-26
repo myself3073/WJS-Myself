f8330aa67c47b90fa4cfea2f33ba538e1b07227b
myself3073 <myself3073@163.com>
2019-03-26 09:19:06 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+
+var gulp = require('gulp'),
+	
+	less = require('gulp-less'),
+
+
+
+
+
+
+
+
+	imagemin = require('gulp-imagemin');
+
+
+
+//deal with css
+gulp.task('css',function () {
+	return gulp.src('./public/css/index.less')
+				.pipe()
+})
